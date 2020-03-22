c1d92ad6835327248f524a40157105093825cc48
Martin Sigloch <f0i.git@projects.f0i.de>
2020-03-22 14:27:05 +0100

    Add readme and tests

---
+++ README.md
@@ -0,0 +1,40 @@
+# Iso8601
+
+Format a [posix time](https://package.elm-lang.org/packages/elm/time/latest/) to a ISO8601 String.
+
+This package is intended to format time stamps for user interfaces with differnt precission.
+
+## Install
+
+```
+elm install f0i/iso8601
+```
+
+## Examples
+
+See [guide.elm-lang.org/…/time.html](https://guide.elm-lang.org/effects/time.html) for how to get the current time.
+
+
+```elm
+import Iso8601
+import Time exposing (Posix)
+
+...
+
+-- get a Posix time from elm/time
+time =
+    Time.millisToPosix 1584875883199
+
+
+-- this will format the time as "2020-03-22T11:18:03.199"
+milli =
+    Iso8601.toUtcString Iso8601.Milli time
+
+-- this will format the time as "2020-03-22T11:18"
+minute =
+    Iso8601.toUtcString Iso8601.Minute time
+
+-- this will format the time as "11:18:03"
+hourToSecond =
+    Iso8601.toUtcString Iso8601.HourSecond time
+```
---
+++ tests/Iso8601Test.elm
@@ -0,0 +1,65 @@
+module Iso8601Test exposing (..)
+
+import Expect exposing (Expectation)
+import Fuzz exposing (Fuzzer, int, list, string)
+import Iso8601
+import Test exposing (..)
+import Time exposing (Posix)
+
+
+time =
+    Time.millisToPosix 1584875883199
+
+
+suite : Test
+suite =
+    describe "The Iso8601 module"
+        [ describe "Format posix time"
+            -- Nest as many descriptions as you like.
+            [ test "epoch" <|
+                \_ ->
+                    Time.millisToPosix 0
+                        |> Iso8601.toUtcString Iso8601.Milli
+                        |> Expect.equal "1970-01-01T00:00:00.000"
+            , test "Year" <|
+                \_ ->
+                    time
+                        |> Iso8601.toUtcString Iso8601.Year
+                        |> Expect.equal "2020"
+            , test "Month" <|
+                \_ ->
+                    time
+                        |> Iso8601.toUtcString Iso8601.Month
+                        |> Expect.equal "2020-03"
+            , test "Day" <|
+                \_ ->
+                    time
+                        |> Iso8601.toUtcString Iso8601.Day
+                        |> Expect.equal "2020-03-22"
+            , test "Hour" <|
+                \_ ->
+                    time
+                        |> Iso8601.toUtcString Iso8601.Hour
+                        |> Expect.equal "2020-03-22T11"
+            , test "Minute" <|
+                \_ ->
+                    time
+                        |> Iso8601.toUtcString Iso8601.Minute
+                        |> Expect.equal "2020-03-22T11:18"
+            , test "Second" <|
+                \_ ->
+                    time
+                        |> Iso8601.toUtcString Iso8601.Second
+                        |> Expect.equal "2020-03-22T11:18:03"
+            , test "Millisecond" <|
+                \_ ->
+                    time
+                        |> Iso8601.toUtcString Iso8601.Milli
+                        |> Expect.equal "2020-03-22T11:18:03.199"
+            , test "Hour to Millisecond" <|
+                \_ ->
+                    time
+                        |> Iso8601.toUtcString Iso8601.HourMilli
+                        |> Expect.equal "11:18:03.199"
+            ]
+        ]
